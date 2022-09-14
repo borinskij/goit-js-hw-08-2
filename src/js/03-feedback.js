d1305f2c3e9954c6ae7eff7b015e187a117750aa
Abdrii Borinskij <aborinskij@gmail.com>
2022-09-14 22:35:17 +0300

    3zadacha

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,65 @@
 
+
+import Throttle from "lodash.throttle" 
+ 
+    const form = document.querySelector(".feedback-form"); 
+    const email = form.querySelector("[name=email]"); 
+    const message = form.querySelector("[name=message]"); 
+ 
+const localKey = "feedback-form-state" 
+ 
+form.addEventListener("input", Throttle(storageFromData, 500)); 
+form.addEventListener("submit", onFormSubmit); 
+ 
+function onFormSubmit(event){ 
+    event.preventDefault(); 
+    const {email, message}=event.currentTarget.elements 
+    console.dir({email: email.value, message:message.value}) 
+     
+    localStorage.removeItem(localKey); 
+    email.value = ""; 
+    message.value = ""; 
+ 
+} 
+ 
+function storageFromData(event){ 
+    const formValue = {email:"", message:""}; 
+ 
+    if(localStorage.getItem(localKey)){ 
+        Object.assign(formValue, JSON.parse(localStorage.getItem(localKey))) 
+    } 
+ 
+    formValue[event.target.name] = event.target.value; 
+ 
+ 
+ 
+    localStorage.setItem(localKey, JSON.stringify(formValue)) 
+}
+
+
+// const refs = {
+//     form: document.querySelector('.feedback-form'),
+//     email: document.querySelector('[name="email"]'),
+//     message: document.querySelector('[name="message"]'),
+// }
+// const{form, email,message} = refs
+
+// const localKey = "feedback-form-state";
+
+// form.addEventListener('input', onSrtorageFormData);
+
+// for
+
+// function onSrtorageFormData(event) {
+//     // console.dir(event.target)
+//     const formValue = { email: '', message: '' };
+//     if (localStorage.getItem(localKey)) {
+//     Object.assign(formValue, JSON.parse(localStorage.getItem(localKey))
+// )}
+
+//     formValue[event.target.name] = event.target.value;
+//     console.log(formValue)
+
+
+//     localStorage.setItem(localKey, JSON.stringify(formValue));
+// }
